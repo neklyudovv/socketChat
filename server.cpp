839fa9f23f6bb9af4392a4a39fba23f3a09df97a
Igor Nekludov <igor.nekludovv@gmail.com>
2025-04-26 19:21:52 +0300

    conn error handling

--- server.cpp
+++ server.cpp
@@ -5,12 +5,22 @@
 
 int main(){
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (serverSocket < 0) {
+        std::cout << "establishing conn error";
+        exit(0);
+    }
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(8080);
     serverAddress.sin_addr.s_addr = INADDR_ANY;
 
-    bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+    int ret = bind(serverSocket, (struct sockaddr *) &serverAddress, sizeof(serverAddress));
+    if (ret < 0) {
+        std::cout << "error binding conn" << std::endl;
+        return -1;
+    }
+
     listen(serverSocket, 5);
 
     int clientSocket = accept(serverSocket, nullptr, nullptr);
