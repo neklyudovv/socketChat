7256aeb31821890a6f4db89d8f06e9fb5aa485f6
Igor Nekludov <igor.nekludovv@gmail.com>
2025-05-01 23:53:35 +0300

    client now handles messaging in 2 threads: receiving and sending

--- client.cpp
+++ client.cpp
@@ -2,6 +2,38 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <thread>
+
+#define CLNT_MSG "[Client] "
+
+bool run = 1;
+
+void receiveMsg(int clientSocket){
+    char buffer[1024];
+    while(run){
+        ssize_t bytes = recv(clientSocket, buffer, sizeof(buffer), 0);
+        if(bytes <= 0) {
+            std::cout << CLNT_MSG << "lost conn\n";
+            run = 0;
+        } else{
+            buffer[bytes] = '\0';
+            std::cout << "Client: " << buffer << "\n";
+        }
+    }
+}
+
+void sendMsg(int clientSocket){
+    std::string message;
+    while(run) {
+        std::cout << "You: ";
+        std::getline(std::cin, message);
+        ssize_t bytesSent = send(clientSocket, message.c_str(), message.size(), 0);
+        if (bytesSent <= 0) {
+            std::cout << CLNT_MSG << "cant send msg";
+            run = 0;
+        }
+    }
+}
 
 int main(){
     int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
@@ -13,18 +45,11 @@ int main(){
 
     connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
 
-    std::cout << "Client: ";
-    std::string message;
-    std::getline(std::cin, message);
+    std::thread receive(receiveMsg, clientSocket);
+    std::thread send(sendMsg, clientSocket);
 
-    ssize_t bytesSent = send(clientSocket, message.c_str(), strlen(message.c_str()), 0);
-
-    while (bytesSent > 0) {
-        std::cout << "Client: ";
-        std::getline(std::cin, message);
-
-        bytesSent = send(clientSocket, message.c_str(), message.length(), 0);
-    }
+    receive.join();
+    send.join();
     close(clientSocket);
     return 0;
 }
