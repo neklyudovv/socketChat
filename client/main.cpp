0e2ff69ecfcfe62b0bfe217a83d941b29d266c57
Igor Nekludov <igor.nekludovv@gmail.com>
2025-05-04 22:11:15 +0300

    add enter username popup

--- client/main.cpp
+++ client/main.cpp
@@ -1,10 +1,17 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QInputDialog>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    bool ok;
+    QString name = QInputDialog::getText(nullptr, "socket chat", "enter name:", QLineEdit::Normal, "", &ok);
+
+    if(!ok || name.trimmed().isEmpty())
+        return 0;
 
     int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -14,6 +21,9 @@ int main(int argc, char *argv[])
     serverAddress.sin_addr.s_addr = INADDR_ANY;
 
     connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+
+    std::string nameStr = name.toUtf8().constData();
+    send(clientSocket, nameStr.c_str(), nameStr.size(), 0);
 
     MainWindow w(clientSocket);
     w.show();
